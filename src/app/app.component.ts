9bde8e72ee6028adab852c604a560acca4ba1960
David <xy.lin@8888play.com>
2018-08-24 12:33:21 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { List } from 'immutable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'arraytest';
+  timer;
+  time = new Date();
+  array;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+    this.array = List([
+      'item1',
+      'item2',
+      'item3',
+      'item4',
+      'item5',
+    ]).groupBy(() => { return; });
+  }
+
 }
